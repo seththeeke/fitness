9508ce8b25173a9b14a96c9ecc9c197e01accb4e
Seth Theeke <Seth.P.Theeke@leidos.com>
2017-04-06 21:44:58 -0400

    Added test component

--- fitness/src/App.js
+++ fitness/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from './TestComponent';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           This is my first attempt at creating a react app! YAYAYAYAYAY!
         </p>
+        <TestComponent />
       </div>
     );
   }
